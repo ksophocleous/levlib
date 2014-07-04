224f002630d1c40586bf37bc58b00a60fd12b251
Konstantinos <ksophocleous@gmail.com>
2014-07-04 14:44:32 +0200

    idle object added

---
+++ include/lev/idle.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "loop.h"
+
+namespace lev
+{
+	class idle
+	{
+		public:
+			idle(lev::loop& loop);
+
+			std::function<void()> on_idle;
+
+			void set_active(bool active);
+
+			friend void handle_event(ev::idle& w, int revent);
+
+		private:
+			idle(const idle&);
+			idle& operator= (const idle&);
+
+			ev::idle _idle;
+	};
+};
---
+++ src/idle.cpp
@@ -0,0 +1,27 @@
+#include <lev/idle.h>
+
+namespace lev
+{
+	idle::idle(lev::loop& loop) : _idle(loop.raw())
+	{
+		_idle.set<handle_event>(this);
+	}
+
+	void idle::set_active(bool active)
+	{
+		if (active)
+			_idle.start();
+		else
+			_idle.stop();
+	}
+
+	//friend 
+	void handle_event(ev::idle& w, int revent)
+	{
+		idle* local_idle = reinterpret_cast<idle*>(w.data);
+		if (local_idle->on_idle)
+			local_idle->on_idle();
+		else
+			local_idle->set_active(false);
+	}
+};
